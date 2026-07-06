77f4684d8bb1fb8cd9d0ec8436495c263a983559
Isaac Juma <francisisaiah01@gmail.com>
2026-07-07 01:20:53 +0300

    changed the navigation bar for smooth UX and UI

--- app/components/Tailwindcss_Ui_Design/menus/NavMenu.vue
+++ app/components/Tailwindcss_Ui_Design/menus/NavMenu.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <nav class="Menu-nav flex flex-col justify-center gap-4
+    <nav class="Menu-nav flex justify-center place-items-center gap-4
         md:flex md:flex-row justify-between md:gap-4
         lg:flex lg:gap-8 lg:justify-around lg:items-center
     
--- app/components/navigation.vue
+++ app/components/navigation.vue
@@ -16,27 +16,22 @@ const navLinks = ref([
 // or any other content i want to display within the navigation menu here. 
 <template>
     <NavMenu>
-        <img @click="isMenuOpen = !isMenuOpen"
-            src="../assets/menu.gif"
+        <NuxtImg @click="isMenuOpen = !isMenuOpen"
+            src="/menu.gif"
             alt="Menu"
-            class="nav-menu-icon cursor-pointer 
-                w-16 h-16
-                md:hidden 
-                lg:hidden
-            "
+            class="nav-menu-icon cursor-pointer relative z-50 block focus:outline-none w-16 h-16 md:hidden lg:hidden"
         />
-        <ul :class="[isMenuOpen ? 'flex' : 'hidden',
-            'Menu-nav flex',
-            'flex-col gap-4',
-            'md:flex md:flex-row md:justify-between md:items-center',
-            'lg:flex lg:justify-between lg:items-center']
+
+        <ul :class="[isMenuOpen ? 'flex bg-blue-300 m-4 p-4 rounded-xl w-full h-auto' : 'hidden md:flex lg:flex',
+            'fixed inset-0 z-40 flex-col items-center justify-center gap-4 backdrop-blur-sm duration-300 ease-in-out',
+            'md:static md:inset-auto md:z-auto md:w-auto md:h-auto md:flex md:flex-row md:justify-between md:items-center md:backdrop-blur-none',
+            'lg:static lg:inset-auto lg:z-auto lg:w-auto lg:h-auto lg:flex lg:justify-between lg:items-center lg:backdrop-blur-none']
         ">
             <li v-for="link in navLinks" :key="link.name">
-                <NuxtLink :to="link.path" class="nav-link">
+                <NuxtLink :to="link.path" @click="isMenuOpen = false" class="nav-link m-2 p-4">
                     {{ link.label }}
                 </NuxtLink>
             </li>
         </ul>
-
     </NavMenu>
 </template>
