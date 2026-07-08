20eed6ed5b09e29b3d0862b442a6778b0ad2e4d8
Isaac Juma <francisisaiah01@gmail.com>
2026-07-08 17:12:41 +0300

    side panel changes

--- app/components/sidePanel.vue
+++ app/components/sidePanel.vue
@@ -1,17 +1,24 @@
 <script setup lang="ts">
+import BaseButton from './Tailwindcss_Ui_Design/BaseButton.vue';
+
 
 </script>
 
 <template>
-    <h3 class="text-center">WHY US</h3>
-    <div class="sidePanel flex flex-row justify-between m-2 p-2">
+    <h3 class="text-2xl text-center m-2 p-2 text-black">WHY US ?</h3>
+    <p class="text-center">With the help of solo-leveling you are guranteed changes in the first 3 months of fully committed</p>
+    <div class="sidePanel flex flex-col justify-between rounded-xl m-2 p-2">
         <NuxtImg
             src="/landingpage.jpg"
-            class="w-1/2 h-[30dvh] m-2 p-2"
+            class="w-full h-[30dvh]"
         />
-        <p class="text-black text-lg m-2 p-4">
-            Take full responsibilities of your every day Habit  <br>
-            We are here to show you the way .
-        </p>
+        <div class="flex flex-col bg-gray-3oo rounded-xl justify-center items-center m-2 p-4">
+            <p class="text-lg m-2">Embrace yourself the way you are on a personal Solo Leveling Journey</p>
+            <p class="bold m-2">Be responsible for your daily Habits We are here to show you the way.</p>
+        </div>
+
+        <BaseButton>
+            Let's Get Started
+        </BaseButton>
     </div>
 </template>
